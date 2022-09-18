6855a42b964b776e96e8bcb3731164eaa837d39e
djpadron <ploys_hulled_0y@icloud.com>
2022-09-17 20:33:31 -0400

    Add reset button

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const grid_container = document.querySelector(".grid-container");
-const total_grid_items = 1000;
+const total_grid_items = 64;
 
 for (let index = 0; index < total_grid_items; index++) {
   let grid_item_div = document.createElement("div");
   grid_item_div.classList.add("grid-item", `grid-item-${index}`);
+
+  grid_item_div.addEventListener("mouseenter", (event) => {
+    event.target.classList.add("mouseenter");
+  });
+
   grid_container.appendChild(grid_item_div);
 }
+
+const resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", (event) => {
+  const grid_items = grid_container.childNodes;
+  grid_items.forEach((grid_item) => {
+    grid_item.classList.remove("mouseenter");
+  });
+});
